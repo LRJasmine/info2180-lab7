39041a0d78e5b9b2bf5f1339de6563c47ead82ff
Ubuntu <ubuntu@ip-172-31-41-7.ec2.internal>
2019-11-15 04:22:47 +0000

    Event Handler for city button

--- world.js
+++ world.js
@@ -1,12 +1,16 @@
 var httpRequest;
 var dataString;
 var response;
-var searchBtn;
+var lookupBtn;
+var citylookupBtn;
 var url ="https://ad26eb2a119e43e89b202de5e7b4bd28.vfs.cloud9.us-east-1.amazonaws.com/world.php";
 window.onload = function (){
     httpRequest = new XMLHttpRequest();
-    searchBtn = document.getElementById("lookup");
-    searchBtn.addEventListener("click", processing); 
+    lookupBtn = document.getElementById("lookup");
+    lookupBtn.addEventListener("click", processing);
+    
+    citylookupBtn = document.getElementById("citylookup");
+    citylookupBtn.addEventListener("click", processingCity);
     
     function processing(event){
         event.preventDefault();
@@ -16,6 +20,16 @@ window.onload = function (){
         httpRequest.open('GET', url);
         httpRequest.send();
     }
+    
+    function processingCity(event){
+        event.preventDefault();
+        dataString = document.getElementById("country").value.trim();
+        url += "?country=" + dataString +"&context=cities";
+        httpRequest.onreadystatechange = forward;
+        httpRequest.open('GET', url);
+        httpRequest.send();
+    }
+    
     function forward(){
         
         if (httpRequest.readyState === XMLHttpRequest.DONE) {
