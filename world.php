e546b3f7ee9e9f3e0ae8a4148e96fa610a8ceeaa
Ubuntu <ubuntu@ip-172-31-41-7.ec2.internal>
2019-11-15 04:23:34 +0000

    City Query lookup

--- world.php
+++ world.php
@@ -11,6 +11,8 @@ $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $p
 <?php 
   if ($_SERVER['REQUEST_METHOD'] === 'GET'){
       $country = filter_input(INPUT_GET,'country',FILTER_SANITIZE_STRING);
+      $city = filter_input(INPUT_GET,'context',FILTER_SANITIZE_STRING);
+      
       if ($country !== ""){
         $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
         $results = $stmt->fetchAll(PDO::FETCH_ASSOC);?>
@@ -25,7 +27,7 @@ $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $p
         </tr>
 
         <?php
-        foreach ($results as $row)
+        foreach ($results as $row):
         {
             echo "<tr>";
             echo "<td>" . $row['name'] ."</td>";
@@ -34,11 +36,42 @@ $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $p
             echo "<td>" . $row['head_of_state'] . "</td>";
             echo "</tr>";
         }
+        endforeach;
         ?>
 
         </table>
         
         <?php
+      }else if ($city == 'cities'){
+          
+        $joinTab = $conn ->query("SELECT countries.name, cities.name, city.district,city.population FROM countries JOIN cities
+        ON countries.code=cities.country_code");
+        $joinResults = $joinTab ->fetchAll(PDO::FETCH_ASSOC); ?>
+        
+        <table border = '2' bgcolor = "white">
+        <tr bgcolor = "skyblue">
+            <th>Country Name</th>
+            <th>City Name</th>
+            <th>District</th>
+            <th>Population</th>
+
+        </tr>
+
+        <?php
+        foreach ($joinResults as $row):
+        {
+            echo "<tr>";
+            echo "<td>" . $row['name'] ."</td>";
+            echo "<td>" . $row['district'] ."</td>";
+            echo "<td>" . $row['population'] ."</td>";
+            echo "</tr>";
+        }
+        endforeach;
+        ?>
+
+        </table>
+        
+      <?php 
       }else{
         $stmt = $conn->query("SELECT * FROM countries");
         $results = $stmt->fetchAll(PDO::FETCH_ASSOC);?>
@@ -52,7 +85,7 @@ $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $p
         </tr>
 
         <?php
-        foreach ($results as $row)
+        foreach ($results as $row):
         {
             echo "<tr>";
             echo "<td>" . $row['name'] ."</td>";
@@ -61,6 +94,7 @@ $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $p
             echo "<td>" . $row['head_of_state'] . "</td>";
             echo "</tr>";
         }
+        endforeach;
         ?>
 
         </table>
