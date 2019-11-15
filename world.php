e4e82ae47616c6914737b5687eb588470e887e2e
Ubuntu <ubuntu@ip-172-31-41-7.ec2.internal>
2019-11-15 00:30:41 +0000

    Results in a table

--- world.php
+++ world.php
@@ -13,21 +13,57 @@ $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $p
       $country = filter_input(INPUT_GET,'country',FILTER_SANITIZE_STRING);
       if ($country !== ""){
         $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
-        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        echo '<ul>';
-        foreach ($results as $row){
-          echo '<li>' . $row['name'] . ' is ruled by ' . $row['head_of_state'] . '</li>';
+        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);?>
+        
+        
+        <table border = '2'>
+        <tr>
+            <th>Country Name</th>
+            <th>Continent</th>
+            <th>Independence Year</th>
+            <th>Head of State</th>
+        </tr>
+
+        <?php
+        foreach ($results as $row)
+        {
+            echo "<tr>";
+            echo "<td>" . $row['name'] ."</td>";
+            echo "<td>" . $row['continent'] . "</td>";
+            echo "<td>" . $row['independence_year'] . "</td>";
+            echo "<td>" . $row['head_of_state'] . "</td>";
+            echo "</tr>";
         }
-          echo '</ul>';
-        }
-      else{
+        ?>
+
+        </table>
+        
+        <?php
+      }else{
         $stmt = $conn->query("SELECT * FROM countries");
         $results = $stmt->fetchAll(PDO::FETCH_ASSOC);?>
-        <ul>
-        <?php foreach ($results as $row): ?>
-        <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-        <?php endforeach; ?>
-        </ul>
+        <table border = '2'>
+        <tr>
+            <th>Country Name</th>
+            <th>Continent</th>
+            <th>Independence Year</th>
+            <th>Head of State</th>
+        </tr>
+
+        <?php
+        foreach ($results as $row)
+        {
+            echo "<tr>";
+            echo "<td>" . $row['name'] ."</td>";
+            echo "<td>" . $row['continent'] . "</td>";
+            echo "<td>" . $row['independence_year'] . "</td>";
+            echo "<td>" . $row['head_of_state'] . "</td>";
+            echo "</tr>";
+        }
+        ?>
+
+        </table>
+        
         <?php
       }
   }
